e73d98b7d8de1e2a101894a09cc25535d320e2da
chetan11234 <148883457+chetan11234@users.noreply.github.com>
2025-08-11 00:57:21 +0530

    Add files via upload

---
+++ task1_math_ops.py
@@ -0,0 +1,6 @@
+num1=float(input("Enter number 1 : "))
+num2=float(input("Enter number 2 : "))
+print("Addition ",num1+num2)
+print("Subtraction ",num1-num2)
+print("Multiplication ",num1*num2)
+print("Division ",num1/num2)
---
+++ task2_greetings.py
@@ -0,0 +1,3 @@
+first_name=str(input("Enter the first name : "))
+last_name=str(input("Enter the last name : "))
+print("Hello, "+first_name+" "+last_name+"! Welcome to python programs")
